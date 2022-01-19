730fc22c28ef3c917f09c7bd708de5b9b17a4c98
Julien PAUSE <julien.pause@epitech.eu>
2022-01-19 23:45:27 +0400

    feat: add icons on bottom tabs

--- mobile/src/navigation/Tabs.tsx
+++ mobile/src/navigation/Tabs.tsx
@@ -1,21 +1,70 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { TabParamList } from './types'
 
 import AreasScreen from '../screens/Areas';
 import AppsScreen from '../screens/Apps';
 import ExploreScreen from '../screens/Explore';
+import { RouteProp } from '@react-navigation/native';
 
 const Tab = createBottomTabNavigator<TabParamList>();
 
+type TabBarIconProps = {
+  focused: boolean,
+  color: string,
+  size: number
+}
+
+function getIcon({ focused, color, size }: TabBarIconProps, iconName: string): React.ReactNode {
+  return (
+    <Ionicons
+      name={focused ? iconName : `${iconName}-outline`}
+      size={size}
+      color={focused ? "#3772FF" : color}
+      style={{
+        backgroundColor: focused ? "rgba(55, 114, 255, 0.1)" : "transparent",
+        padding: 5,
+        borderRadius: 7
+      }}
+    />
+  )
+}
+
 function TabsNavigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Areas" component={AreasScreen} />
-      <Tab.Screen name="Apps" component={AppsScreen} />
-      <Tab.Screen name="Explore" component={ExploreScreen} />
+    <Tab.Navigator screenOptions={options}>
+      <Tab.Screen
+        name="Areas"
+        component={AreasScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, 'infinite')
+        }}
+      />
+      <Tab.Screen
+        name="Apps"
+        component={AppsScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, 'apps')
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarIcon: props => getIcon(props, 'compass')
+        }}
+      />
     </Tab.Navigator>
   )
+}
+
+const options: BottomTabNavigationOptions = {
+  tabBarShowLabel: false,
+  tabBarStyle: {
+    borderTopColor: "black",
+    borderTopWidth: 1
+  }
 }
 
 export default TabsNavigation
