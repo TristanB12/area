c26d95605de709526c0ec75efc5d0821c1bf60ee
luan <luan.sautron@epitech.eu>
2022-02-08 00:55:54 +0400

    fix: change method to refresh an AREA access_token to GET FROM POST

--- back/routes/token/index.js
+++ back/routes/token/index.js
@@ -3,7 +3,7 @@ var router = require('express').Router();
 const tokenController = require('../../controllers/token.controller');
 const tokenMiddleWare = require('../../middleware/token.middleware');
 
-router.post('/refresh', tokenMiddleWare.getRefreshToken, tokenController.refreshAreaAccessToken);
+router.get('/refresh', tokenMiddleWare.getRefreshToken, tokenController.refreshAreaAccessToken);
 
 router.get('/verify', tokenMiddleWare.verifyAccessToken, tokenController.verifyAreaAccessToken);
 
