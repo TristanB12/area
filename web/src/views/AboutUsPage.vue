ea73c9fcb92177b535f6b75c98654b171e25ee2f
noemie cadet <luan.sautron@epitech.eu>
2022-02-09 09:42:40 +0400

    add 'about us' page and 'service page'

--- web/src/views/AboutUsPage.vue
+++ web/src/views/AboutUsPage.vue
@@ -1,6 +1,34 @@
 <template>
+<div class="devIcons">
     <div>
-
+        <img
+            class="julienIcon"
+            src="@/assets/julien.png"
+            alt="Julien - developper mobile front end"
+        />
+        <img
+            class="dorianIcon"
+            src="@/assets/dorian.png"
+            alt="Dorian - developper mobile front end"
+        />
+        <img
+            class="luanIcon"
+            src="@/assets/luan.png"
+            alt="Luan - developper back end"
+        />
+    </div>
+    <div>
+        <img
+            class="tristanIcon"
+            src="@/assets/tristan.png"
+            alt="Tristan - developper web front end"
+        />
+        <img
+            class="noemieIcon"
+            src="@/assets/noemie.png"
+            alt="Noemie - developper web front end"
+        />
+    </div>
     </div>
 </template>
 
@@ -11,5 +39,26 @@
 </script>
 
 <style lang="scss" scoped>
+.julienIcon {
+    margin-top: 20px;
+    margin-left: 8%;
+    height: 300px;
+}
+.dorianIcon {
+    margin-left: 8%;
+    height: 300px;
+}
+.luanIcon {
+    margin-left: 8%;
+    height: 300px;
+}
+.tristanIcon {
+    margin-left: 23%;
+    height: 300px;
+}
+.noemieIcon {
+    margin-left: 10%;
+    height: 300px;
+}
 
 </style>
