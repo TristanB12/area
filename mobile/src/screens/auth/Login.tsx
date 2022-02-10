ff8dae09f4f52297bea9590561130474c41adf39
Julien PAUSE <julien.pause@epitech.eu>
2022-02-10 21:53:46 +0400

    refactor: merge Login and Register into one component

--- mobile/src/screens/auth/Login.tsx
+++ mobile/src/screens/auth/Login.tsx
@@ -1,45 +1,8 @@
 import React from "react";
-import {
-  Text,
-  Heading,
-  VStack,
-  Center,
-  Divider
-} from "native-base";
-import { useTranslation } from "react-i18next";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StackParamList } from "../../navigation/types";
-import AuthServices from "../../components/auth/Services";
-import AuthEmailPassword from "../../components/auth/EmailPassword";
-import authServicesMock from "../../mock/authServices";
+import AuthScreen from "../../components/auth/Screen";
 
-type LoginScreenProps = NativeStackScreenProps<StackParamList, 'Login'>
-
-function Login({ navigation } : LoginScreenProps) {
-  const { t } = useTranslation('auth')
-  const goToRegister = () => navigation.navigate('Register')
-
-  return (
-    <Center w="100%" flex={1} >
-      <VStack safeArea space={8} p="2" w="80%"  py="8">
-        <Heading size="lg" color="primary.400">
-          { t('login').charAt(0) }
-          <Heading size="lg">
-            { t('login').slice(1) }
-          </Heading>
-        </Heading>
-        <AuthServices services={authServicesMock} action="login"/>
-        <Divider />
-        <AuthEmailPassword action="login" />
-        <Text textAlign="center">
-          { t('no_account_yet') + ' ' }
-          <Text color="tertiary.400" bold underline onPress={goToRegister}>
-            { t('register') }
-          </Text>
-        </Text>
-      </VStack>
-    </Center>
-  )
+function LoginScreen() {
+  return <AuthScreen action='login' />
 }
 
-export default Login
+export default LoginScreen
--- mobile/src/screens/auth/Register.tsx
+++ mobile/src/screens/auth/Register.tsx
@@ -1,45 +1,8 @@
 import React from "react";
-import {
-  Text,
-  Heading,
-  VStack,
-  Center,
-  Divider,
-} from "native-base";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StackParamList } from "../../navigation/types";
-import { useTranslation } from "react-i18next";
-import AuthEmailPassword from "../../components/auth/EmailPassword";
-import AuthServices from "../../components/auth/Services";
-import authServicesMock from "../../mock/authServices";
+import AuthScreen from '../../components/auth/Screen'
 
-type RegisterScreenProps = NativeStackScreenProps<StackParamList, 'Register'>
-
-function Register({ navigation } : RegisterScreenProps) {
-  const { t } = useTranslation('auth')
-  const goToLogin = () => navigation.navigate('Login')
-
-  return (
-    <Center w="100%" flex={1}>
-      <VStack safeArea space={6}  w="80%" px="2" py="8">
-        <Heading size="lg" color="primary.400">
-          { t('register').charAt(0) }
-          <Heading size="lg">
-            { t('register').slice(1) }
-          </Heading>
-        </Heading>
-        <AuthServices services={authServicesMock} action='register' />
-        <Divider />
-        <AuthEmailPassword action="register" />
-        <Text textAlign="center">
-          { t('already_an_account') + ' ' }
-          <Text color="tertiary.400" bold underline onPress={goToLogin}>
-            { t('login') }
-          </Text>
-        </Text>
-      </VStack>
-    </Center>
-  )
+function Register() {
+  return <AuthScreen action='register' />
 }
 
 export default Register
