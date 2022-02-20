d9c7e8e03e337d0f8b21013895b259a03e988edb
Julien PAUSE <julien.pause@epitech.eu>
2022-02-20 15:40:35 +0400

    refactor: only keep image on splashScreen

--- mobile/src/screens/Splash.tsx
+++ mobile/src/screens/Splash.tsx
@@ -1,31 +1,18 @@
 import React from "react"
-import { Center, Heading, Image, Text, VStack } from "native-base"
+import { Center, Image } from "native-base"
 import illustration from '../assets/illustration.png'
-import { useTranslation } from "react-i18next"
 
 function SplashScreen() {
-  const { t } = useTranslation('common')
 
   return (
     <Center flex={1}>
-      <VStack space={10} alignItems="center">
-        <Heading size="3xl" color="primary.400">
-          A
-          <Heading size="3xl" color="black">
-            REA
-          </Heading>
-        </Heading>
-        <Image
-          source={illustration}
-          alt="Area Illustration"
-          width="250"
-          height="250"
-          resizeMode="contain"
-        />
-        <Text fontSize="lg">
-          { t('loading') }
-        </Text>
-      </VStack>
+      <Image
+        source={illustration}
+        alt="Area Illustration"
+        width="250"
+        height="250"
+        resizeMode="contain"
+      />
     </Center>
   )
 }
