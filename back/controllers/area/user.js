9eb7ca3257e31eeeaad4c866ab331b9656e66d75
luan <luan.sautron@epitech.eu>
2022-02-24 03:34:28 +0400

    fix: split area part in 3 files (auth, area, user)

---
+++ back/controllers/area/user.js
@@ -0,0 +1,16 @@
+/**
+ * Get user information for area service
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+async function getInfos(req, res) {
+  const { user } = req;
+
+  return res.status(200).json({
+    email: user.auth.email,
+    email_verified: user.email_verified,
+  });
+}
+
+module.exports = { getInfos };
