7ed78e8a6e22fbdb594dddd393b3749143ed331d
luan <luan.sautron@epitech.eu>
2022-02-23 15:01:27 +0400

    feat: add new tools (Sentry) for debugging Error in Back-end

--- back/app.js
+++ back/app.js
@@ -1,11 +1,34 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 const cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 
 var app = express();
 
+Sentry.init({
+  dsn: process.env.SENTRY_DNS,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -13,5 +36,16 @@ app.use(cookieParser());
 app.use(cors());
 
 require('./routes')(app);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 module.exports = app;
