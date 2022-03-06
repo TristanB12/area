da8ff6648b9dc5b2d554428fbd204aa03d22ccdd
noemie-cadet <noemie.cadet@epitech.eu>
2022-03-06 15:35:37 +0400

    layout of mobile page

--- web/src/views/MobilePage.vue
+++ web/src/views/MobilePage.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="mobile-app">
-        <a href="./app-release.apk">
-            <VButton title="Download app"/>
-        </a>
+        <div class="sector">
+            <img class="phone-screen" src="@/assets/Phone.png" alt="Design Image" />
+            <div class="right-side">
+                <h1 class="mobile-text">
+                    Join us on our
+                    <span class="important">mobile app</span>.
+                </h1>
+                <a href="./app-release.apk" class="button">
+                    <VButton title="Download app"/>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,8 +26,34 @@ import VButton from '@/components/ui/VButton.vue';
 
 <style lang="scss" scoped>
 .mobile-app {
-    width: 200px;
-    margin: auto;
-    margin-top: 20vh;
+   display: flex;
+  justify-content: space-between;
+}
+.sector {
+  display: flex;
+  width: 60%;
+  margin: auto;
+  margin-top: 10px;
+}.right-side {
+    margin-top: 100px;
+}
+.important {
+  color: #f06543;
+}
+.phone-screen {
+    margin-top: 50px;
+  max-height: 600px;
+}
+.button {
+    margin-top: 70px;
+}
+.mobile-text {
+  margin-top: 90px;
+  font-family: $primary-font;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 87px;
+  letter-spacing: -1px;
 }
 </style>
